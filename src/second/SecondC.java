84a0e5277b329f4cc1bc10d41495963bd262258c
jsshmaster <wlstn900@naver.com>
2016-05-09 23:54:20 +0900

    마지막
    마지막

--- src/second/SecondC.java
+++ src/second/SecondC.java
@@ -5,6 +5,9 @@ public class SecondC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("¶Ç ¹Ù²Û´Ù ");
+		System.out.println("¶Ç ¹Ù²Û´Ù ");
+		System.out.println("¶Ç ¹Ù²Û´Ù ");
+		System.out.println("¶Ç ¹Ù²Û´Ù ");
 	}
 
 }
